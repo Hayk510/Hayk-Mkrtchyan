cf58251dfc9832969fa631db73839da6685d5c25
Hayk510 <62568600+Hayk510@users.noreply.github.com>
2020-03-23 23:43:01 +0400

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Forms.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Forms
+    {   
+        public Forms() { }
+        public virtual void Create(int left, int top) { }
+        public virtual void Move() { } 
+    }
+}
